6f36ee48408e8c6fd173a4d7c9a0d2ea6eb31e87
Nicolaus Rossi <n.rossi2002@gmail.com>
2020-08-18 01:16:49 +0200

    firebase connection handler

--- src/firebase_connector.ts
+++ src/firebase_connector.ts
@@ -1,16 +1,16 @@
-import { firebaseConfig } from './firebase_config'
-import firebase from 'firebase/app'
-
+import firebase from 'firebase'
+import WebScraper from './webscraper'
 
 /**
- * Handles the connection between app and Firebase.
+ * Handles the connection and data transfer between app and Firebase.
  * 
  * @author Nicolaus Rossi
  * @since 2020-8-17
  */
 export default class FirebaseConnector {
     private static instance: FirebaseConnector
-    private firebaseDatabase = firebase.database()
+    private firebaseDatabase: firebase.database.Database = firebase.database()
+    private webScraper: WebScraper = new WebScraper()
 
     constructor() {
         if (FirebaseConnector.instance) {
@@ -21,5 +21,41 @@ export default class FirebaseConnector {
 
     public static getInstance(): FirebaseConnector {
         return FirebaseConnector.instance
+    }
+
+    /**
+     * Appends the price and date (both at the time of execution) to the data tab of the passed item in the database.
+     * @param jsonItemName the item's name in the database.
+     */
+    public writeItemData(jsonItemName: string): void {
+        this.getItemUrl(jsonItemName).then((url: string) => {
+            this.webScraper.fetchItemPrice(url).then((price) => {
+                this.firebaseDatabase.ref(`tracked-items/${jsonItemName}/data`).child(this.getFormattedDate()).set(price)
+            })
+        })
+    }
+
+    /**
+     * Gets the Amazon url to the passed item.
+     * @param jsonItemName the item's name in the database.
+     */
+    private getItemUrl(jsonItemName: string) {
+        return new Promise((resolve, reject) => {
+            this.firebaseDatabase.ref(`tracked-items/${jsonItemName}/url`)
+                .once('value')
+                .then((snapshot) => {
+                    resolve(snapshot.val())
+                })
+                .catch((error) => reject(error))
+        })
+    }
+    
+    /**
+     * Returns the current date in dd-MM-YYYY format.
+     * @returns the formatted date.
+     */
+    private getFormattedDate(): string {
+        const date = new Date()
+        return [date.getFullYear(), date.getMonth() + 1, date.getDate()].join('-')
     }
 }
