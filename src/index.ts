1cea063357af404cb9b29d143e00863887fefc00
Nicolaus Rossi <n.rossi2002@gmail.com>
2020-08-18 16:47:57 +0200

    tests

--- src/index.ts
+++ src/index.ts
@@ -5,4 +5,8 @@ import FirebaseConnector from './firebase_connector'
 firebase.initializeApp(firebaseConfig)
 
 const firebaseConnector = new FirebaseConnector()
-FirebaseConnector.getInstance().writeItemData('samsung-ultrawide-gaming-monitor')
+// FirebaseConnector.getInstance().writeItemData('samsung-ultrawide-gaming-monitor')
+// FirebaseConnector.getInstance().addItemToTrackingList('samsung-ultrawide-gaming-monitor', '').then((items) => console.log(items))
+
+// FirebaseConnector.getInstance().getTrackedItemKeys().then((items) => console.log(items))
+FirebaseConnector.getInstance().getTrackedItems().then((items) => console.log(typeof(items)))
