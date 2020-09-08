44ea5b0cc23f4fd04996736dc7391f8918b463a3
Nicolaus Rossi <n.rossi2002@gmail.com>
2020-09-08 14:17:47 +0200

    revamped typing

--- page/firebase_fetch.py
+++ page/firebase_fetch.py
@@ -16,7 +16,7 @@ class FireBaseFetch:
         })
         self.__firebase_database = database
 
-    def get_instance(self) -> FireBaseFetch:
+    def get_instance(self):
         return FireBaseFetch.__instance
 
     def get_item_data(self, json_item_name: str) -> Dict[str, str]: 
