253507058cd50ef3019a35039a047e27c13196f1
Nicolaus Rossi <n.rossi2002@gmail.com>
2020-09-10 15:40:55 +0200

    type coersion

--- src/ts/webscraper.ts
+++ src/ts/webscraper.ts
@@ -32,7 +32,7 @@ export default class WebScraper {
                 .then((response) => {
                     let $ = cheerio.load(response.data)
                     let price: Cheerio = $('#priceblock_ourprice')
-                    resolve(price.text() != '' ? price.text() : '00,00 €')
+                    resolve(price.text() !== '' ? price.text() : '00,00 €')
                 })
                 .catch((error) => reject(error))
         })
